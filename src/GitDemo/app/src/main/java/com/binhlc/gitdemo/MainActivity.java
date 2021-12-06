a8019df1236e7893c72690fa14cf6067fe2d8d4b
Lê Công Bình <congbinh21012001@gmail.com>
2021-12-06 20:41:17 +0700

    Create conflict

--- src/GitDemo/app/src/main/java/com/binhlc/gitdemo/MainActivity.java
+++ src/GitDemo/app/src/main/java/com/binhlc/gitdemo/MainActivity.java
@@ -13,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        textView = findViewById(R);
     }
 }
