2a61c423b70afecbb5117ff8cfc659e8289d0fe8
hoang1645 <60732952+hoang1645@users.noreply.github.com>
2021-12-16 20:29:17 +0700

    Add: category enumeration

---
+++ src/app/src/main/java/com/hcmus/group14/moneytor/services/options/Category.java
@@ -0,0 +1,19 @@
+package com.hcmus.group14.moneytor.data.model;
+
+
+enum Category
+{
+    FOOD_AND_DRINK,
+    TRAFFIC,
+    BILLS,
+    MAINTENANCE,
+    MEDICAL,
+    HEALTH,
+    EDUCATION,
+    HOUSEWARE,
+    PETS,
+    UTILITIES,
+    FITNESS,
+    MAKEUP,
+    ENTERTAINMENT
+}
