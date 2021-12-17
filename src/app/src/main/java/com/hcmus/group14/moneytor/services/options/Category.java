cde6cff26c4566a4c33a0eb50cef8c029017f2a2
hoang1645 <60732952+hoang1645@users.noreply.github.com>
2021-12-17 08:41:03 +0700

    Modify: Debt/lend details setters - added != conditions

--- src/app/src/main/java/com/hcmus/group14/moneytor/services/options/Category.java
+++ src/app/src/main/java/com/hcmus/group14/moneytor/services/options/Category.java
@@ -1,19 +1,18 @@
-package com.hcmus.group14.moneytor.data.model;
+package com.hcmus.group14.moneytor.services.options;
 
 
 enum Category
 {
-    FOOD_AND_DRINK,
-    TRAFFIC,
-    BILLS,
-    MAINTENANCE,
-    MEDICAL,
-    HEALTH,
-    EDUCATION,
-    HOUSEWARE,
-    PETS,
-    UTILITIES,
-    FITNESS,
-    MAKEUP,
-    ENTERTAINMENT
+    FOOD_AND_DRINK, //Food and drink related
+    TRAFFIC,        //For vehicles, gasoline,...
+    BILLS,          //Cyclical bills (electricity, water, internet, Netflix...)
+    MAINTENANCE,    //House/vehicle maintenance
+    HEALTH,         //Healthcare (drugs 'n stuff)
+    EDUCATION,      //Education and study related
+    APPLIANCES,     //Household appliances (TV, fridge,...)
+    PETS,           //Related to pets (if available), stuff like food, medical expenses...
+    UTILITIES,      //Other stuff related to household (internet installment...)
+    FITNESS,        //Gym, aerobics, etc.
+    MAKEUP,         //Beauty expenses
+    ENTERTAINMENT   //For fun stuff
 }
