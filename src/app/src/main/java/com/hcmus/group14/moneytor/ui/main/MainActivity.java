cc3db38c29ad458c2ea639de4364ff186664f780
tienthanh214 <39771202+tienthanh214@users.noreply.github.com>
2021-12-17 09:56:23 +0700

    add viewmodel: spending details, input validation

--- src/app/src/main/java/com/hcmus/group14/moneytor/ui/main/MainActivity.java
+++ src/app/src/main/java/com/hcmus/group14/moneytor/ui/main/MainActivity.java
@@ -1,12 +1,15 @@
 package com.hcmus.group14.moneytor.ui.main;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.hcmus.group14.moneytor.R;
+import com.hcmus.group14.moneytor.data.model.Spending;
+import com.hcmus.group14.moneytor.services.spending.SpendingViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +17,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SpendingViewModel viewModel = new ViewModelProvider(this).get(SpendingViewModel.class);
+        Spending spending = new Spending(1234, "Thanh", "no",
+                "no des", 12312312312L);
+        spending.setSpendingId(1);
+//        viewModel.insertSpending(spending);
+        viewModel.deleteSpending(spending);
     }
+
 }
