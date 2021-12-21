2486258260630776ec74b84855890237f69c59e3
hoang1645 <60732952+hoang1645@users.noreply.github.com>
2021-12-21 20:42:49 +0700

    Add: temporary ViewModel for analysis

--- src/app/src/main/java/com/hcmus/group14/moneytor/services/analyze/AnalyzeViewModel.java
+++ src/app/src/main/java/com/hcmus/group14/moneytor/services/analyze/AnalyzeViewModel.java
@@ -18,31 +18,19 @@ import java.util.List;
 
 //TODO: proper parameterization
 public class AnalyzeViewModel extends AndroidViewModel {
-    private List<Spending> allSpending;
-
-    private static final long DAILY = 1000 * 24 * 60 * 60;
-    public static final long WEEKLY = 7 * DAILY;
-    public static final long MONTHLY = 30 * DAILY;
-    public static final long ANNUALLY = 365 * DAILY;
+//    private List<Spending> allSpending;
+//
+//    private static final long DAILY = 1000 * 24 * 60 * 60;
+//    public static final long WEEKLY = 7 * DAILY;
+//    public static final long MONTHLY = 30 * DAILY;
+//    public static final long ANNUALLY = 365 * DAILY;
 
     public AnalyzeViewModel(@NonNull Application application) {
         super(application);
     }
 
-//    public ArrayList<Spending> getSpendingDetails(L)
-//    {
-//        long now = DateTimeUtils.getCurrentTimeMillis();
-//        assert(duration == WEEKLY ||
-//                duration == MONTHLY || duration == ANNUALLY);
-//        ArrayList<Spending> spendingInDuration = new ArrayList<>();
-//        for (Spending spending: allSpending)
-//        {
-//            if (now - spending.getDate() <= duration)
-//                spendingInDuration.add(spending);
-//        }
-//        return spendingInDuration;
-//    }
 
+    //Get total amount of spendings filtered
     public long getTotal(List<Spending> spendings)
     {
         long sum = 0l;
@@ -51,6 +39,7 @@ public class AnalyzeViewModel extends AndroidViewModel {
         return sum;
     }
 
+    //Get the average
     public long getAverage(List<Spending> spendings)
     {
         long sum = 0l;
@@ -59,6 +48,7 @@ public class AnalyzeViewModel extends AndroidViewModel {
         return sum / (long)spendings.size();
     }
 
+    //Get the highest spending across the filtered.
     public long getMaxSpending(List<Spending> spendings)
     {
         long max = 0;
@@ -68,13 +58,14 @@ public class AnalyzeViewModel extends AndroidViewModel {
         return max;
     }
 
+    //Count how much have been spent in categories in appearance across all spendings filtered.
     public HashMap<Category, Long> getDetailsForCategories
             (List<Spending> spendings)
     {
         HashMap<Category, Long> returnResult = new HashMap<>();
 
         ArrayList<Category> categories = new ArrayList<>();
-
+        //Digs all categories available in all filtered spendings
         for (Spending spending : spendings) {
             try {
                 if (!categories.contains(Category.valueOf(spending.getCategory())))
@@ -83,7 +74,7 @@ public class AnalyzeViewModel extends AndroidViewModel {
                 continue;
             }
         }
-
+        //Binds categories and their corresponding amount spent to the map
         for (Category category : categories)
         {
             long val = 0l;
