6d77731f6a02357d65ed448d606d9d3d2340df90
binhlecong <congbinh21012001@gmail.com>
2021-12-22 20:06:22 +0700

    feat: config firebase for google auth

---
+++ src/app/src/main/java/com/hcmus/group14/moneytor/ui/login/LoginActivity.java
@@ -0,0 +1,23 @@
+package com.hcmus.group14.moneytor.ui.login;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.hcmus.group14.moneytor.R;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void loginWithGoogleAccount(View view) {
+    }
+
+    public void loginWithLocalAccount(View view) {
+
+    }
+}
