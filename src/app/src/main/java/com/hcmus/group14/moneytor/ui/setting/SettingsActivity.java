de360b1157875494dd7dcb7e5f57c4234629168f
binhlecong <congbinh21012001@gmail.com>
2021-12-24 01:32:52 +0700

    feat: display user info in SettingsActivity

--- src/app/src/main/java/com/hcmus/group14/moneytor/ui/setting/SettingsActivity.java
+++ src/app/src/main/java/com/hcmus/group14/moneytor/ui/setting/SettingsActivity.java
@@ -1,14 +1,21 @@
 package com.hcmus.group14.moneytor.ui.setting;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.hcmus.group14.moneytor.R;
+import com.hcmus.group14.moneytor.utils.PreferenceUtils;
 
 public class SettingsActivity extends AppCompatActivity {
+    private ImageView ivPhoto;
+    private TextView tvUsername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +31,29 @@ public class SettingsActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+        // Show profile picture and name
+        displayUserInfo();
+    }
+
+    private void displayUserInfo() {
+        ivPhoto = findViewById(R.id.iv_photo);
+        tvUsername = findViewById(R.id.tv_username);
+        // Set user profile photo
+        Uri photoUri = Uri.parse(PreferenceUtils.getString(
+                PreferenceUtils.getInstance(this, "user_profile"),
+                "user_name",
+                "android.resource://com.hcmus.group14.moneytor/" + R.drawable.account_circle_fill
+        ));
+        ivPhoto.setImageURI(photoUri);
+        // Set user name
+        tvUsername.setText(PreferenceUtils.getString(
+                PreferenceUtils.getInstance(this, "user_profile"),
+                "user_name",
+                "New user"));
+    }
+
+    void logout(View view) {
+        //FirebaseAuth.getInstance().signOut();
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
