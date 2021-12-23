f105078b190035f90aa5a26071fdb360c1e5efe9
binhlecong <congbinh21012001@gmail.com>
2021-12-23 22:20:09 +0700

    feat: add setting button in main activity

--- src/app/src/main/java/com/hcmus/group14/moneytor/ui/main/MainActivity.java
+++ src/app/src/main/java/com/hcmus/group14/moneytor/ui/main/MainActivity.java
@@ -2,8 +2,12 @@ package com.hcmus.group14.moneytor.ui.main;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.hcmus.group14.moneytor.R;
@@ -25,18 +29,36 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void onOpenSpendingGoal(View view){
+    public void onOpenSpendingGoal(View view) {
         Intent intent = new Intent(this, GoalActivity.class);
         startActivity(intent);
     }
 
-    public void onOpenDebtLend(View view){
+    public void onOpenDebtLend(View view) {
         Intent intent = new Intent(this, DebtLendActivity.class);
         startActivity(intent);
     }
 
-    public void onOpenAnalysis(View view){
+    public void onOpenAnalysis(View view) {
         Intent intent = new Intent(this, AnalysisActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.actionSetting:
+
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
