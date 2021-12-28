09c06ffe4a692bc32c92f3fd2c14177d806e6214
tienthanh214 <39771202+tienthanh214@users.noreply.github.com>
2021-12-28 22:27:45 +0700

    feature bubble widget - show when note notification

--- src/app/src/main/java/com/hcmus/group14/moneytor/services/notification/receiver/NoteBroadcastReceiver.java
+++ src/app/src/main/java/com/hcmus/group14/moneytor/services/notification/receiver/NoteBroadcastReceiver.java
@@ -3,14 +3,20 @@ package com.hcmus.group14.moneytor.services.notification.receiver;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.Icon;
+import android.os.Build;
 
 import androidx.core.app.NotificationCompat;
+import androidx.core.graphics.drawable.IconCompat;
 
 import com.hcmus.group14.moneytor.R;
+import com.hcmus.group14.moneytor.ui.spending.AddSpendingActivity;
+import com.hcmus.group14.moneytor.ui.widget.BubbleActivity;
 
 public class NoteBroadcastReceiver extends BroadcastReceiver {
     public static final String CHANNEL_ID = "note_reminder_channel";
@@ -23,13 +29,25 @@ public class NoteBroadcastReceiver extends BroadcastReceiver {
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
         NotificationChannel notificationChannel = null;
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             notificationChannel = new NotificationChannel(CHANNEL_ID, channelName,
-                    NotificationManager.IMPORTANCE_DEFAULT);
-            notificationChannel.setLightColor(Color.BLUE);
+                    NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.setLightColor(Color.RED);
             notificationChannel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
             notificationManager.createNotificationChannel(notificationChannel);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                notificationChannel.setAllowBubbles(true);
+            }
         }
+        // TODO: bubble WIDGET
+        Intent target = new Intent(context, BubbleActivity.class);
+        PendingIntent bubbleIntent = PendingIntent.getActivity(context, 0, target, 0);
+        NotificationCompat.BubbleMetadata bubbleData = new NotificationCompat.BubbleMetadata.Builder(
+                bubbleIntent, IconCompat.createWithResource(context, R.drawable.ic_app_logo))
+                .setDesiredHeight(600)
+                .setAutoExpandBubble(false)
+                .setSuppressNotification(true)
+                .build();
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
                 .setContentTitle(context.getString(R.string.reminder_title))
@@ -38,7 +56,8 @@ public class NoteBroadcastReceiver extends BroadcastReceiver {
                 .setCategory(NotificationCompat.CATEGORY_REMINDER)
                 .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setStyle(new NotificationCompat.BigTextStyle());
+                .setStyle(new NotificationCompat.BigTextStyle())
+                .setBubbleMetadata(bubbleData);
 
         // TODO set what happen when press notification
 //                Intent notificationIntent = new Intent(Intent.ACTION_VIEW,
@@ -48,6 +67,8 @@ public class NoteBroadcastReceiver extends BroadcastReceiver {
 //                builder.setContentText(alarm.getDescription() + " - Tap to open url: " + alarm
 //                .getTagUri());
 //                builder.setContentIntent(contentIntent);
+
+
 
         notificationManager.notify(notifId, builder.build());
     }
